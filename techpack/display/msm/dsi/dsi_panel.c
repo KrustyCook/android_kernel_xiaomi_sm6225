CHRISL7/kernel_xiaomi_sm6225

techpack: display: Implement screen state tracking for exposure adjustment
- This is done to skip exposure adjustment sysfs node changes when the screen is off.
- This fixes a BSOD (Black screen of death) when exposure adjustment is turned on at screen off.

Change-Id: I6880cabdc858fc143793bb2bdc7e6d931929220c
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
[muralivijay: Adapt to spes]
Signed-off-by: muralivijay <muralidharan9845@gmail.com>
Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
Signed-off-by: GitHub <noreply@github.com>

techpack/display/msm/dsi/dsi_panel.c
@@ -17,6 +17,7 @@
 #include "sde_dbg.h"
 
 #ifdef CONFIG_TARGET_PROJECT_K7T
+#include <linux/module.h>
 #include "exposure_adjustment.h"
 #endif
 
@@ -39,6 +40,10 @@
 #define MIN_PREFILL_LINES      35
 
 extern void lcd_esd_enable(bool on);
+
+#ifdef CONFIG_TARGET_PROJECT_K7T
+static bool screen_on = true;
+#endif
 
 enum dsi_dsc_ratio_type {
 	DSC_8BPC_8BPP,
@@ -3629,6 +3634,9 @@ static ssize_t mdss_fb_set_ea_enable(struct device *dev,
 {
 	u32 ea_enable;
 
+	if (!screen_on)
+		return len;
+
 	if (sscanf(buf, "%d", &ea_enable) != 1) {
 		DSI_ERR("sccanf buf error!\n");
 		return len;
@@ -4881,3 +4889,61 @@ int dsi_panel_apply_hbm_mode(struct dsi_panel *panel)
 
 	return rc;
 }
+
+#ifdef CONFIG_TARGET_PROJECT_K7T
+static int dsi_panel_dc_dim_notifier_callback(struct notifier_block *self, unsigned long event, void *data)
+{
+	struct drm_notify_data *evdata = data;
+	unsigned int blank;
+
+	if (event != DRM_EVENT_BLANK)
+		return 0;
+
+	if (evdata && evdata->data) {
+		blank = *(int *)(evdata->data);
+		switch (blank) {
+		case DRM_BLANK_POWERDOWN:
+			if (!screen_on)
+				break;
+			screen_on = false;
+			break;
+		case DRM_BLANK_UNBLANK:
+			if (screen_on)
+				break;
+			screen_on = true;
+			break;
+		default:
+			break;
+		}
+	}
+
+	return NOTIFY_OK;
+}
+
+static struct notifier_block dsi_panel_dc_dim_notifier = {
+	.notifier_call = dsi_panel_dc_dim_notifier_callback,
+};
+
+static int __init dsi_panel_dc_dim_init(void)
+{
+
+	int ret = 0;
+
+	// Register the driver module as a client of the DRM PANEL event notifier
+	ret = drm_register_client(&dsi_panel_dc_dim_notifier);
+
+	if (ret)
+		DSI_ERR("Failed to register notifier, err: %d\n", ret);
+
+	return ret;
+}
+
+static void __exit dsi_panel_dc_dim_exit(void)
+{
+	// Unregister the driver module as a client of the DRM PANEL event notifier
+	drm_unregister_client(&dsi_panel_dc_dim_notifier);
+}
+
+module_init(dsi_panel_dc_dim_init);
+module_exit(dsi_panel_dc_dim_exit);
+#endif

KrustyCook/android_kernel_xiaomi_sm6225

Signed-off-by: mrsrimar22 <mrsrimar22@gmail.com>

techpack/display/msm/dsi/dsi_panel.c
@@ -17,6 +17,7 @@
 #include "sde_dbg.h"
 
 #ifdef CONFIG_DSI_EXP_ADJ
+#include <linux/module.h>
 #include "exposure_adjustment.h"
 #endif
 
@@ -39,6 +40,10 @@
 #define MIN_PREFILL_LINES      35
 
 extern void lcd_esd_enable(bool on);
+
+#ifdef CONFIG_DSI_EXP_ADJ
+static bool screen_on = true;
+#endif
 
 enum dsi_dsc_ratio_type {
 	DSC_8BPC_8BPP,
@@ -3614,6 +3619,9 @@ static ssize_t mdss_fb_set_ea_enable(struct device *dev,
 {
 	u32 ea_enable;
 
+	if (!screen_on)
+		return len;
+
 	if (sscanf(buf, "%d", &ea_enable) != 1) {
 		DSI_ERR("sccanf buf error!\n");
 		return len;
@@ -4702,6 +4710,63 @@ int dsi_panel_apply_hbm_mode(struct dsi_panel *panel)
 
 	return rc;
 }
+
+#ifdef CONFIG_DSI_EXP_ADJ
+static int dsi_panel_dc_dim_notifier_callback(struct notifier_block *self,
+			unsigned long event, void *data)
+{
+	struct drm_notify_data *evdata = data;
+	unsigned int blank;
+
+	if (event != DRM_EVENT_BLANK)
+		return 0;
+
+	if (evdata && evdata->data) {
+		blank = *(int *)(evdata->data);
+		switch (blank) {
+		case DRM_BLANK_POWERDOWN:
+			if (!screen_on)
+				break;
+			screen_on = false;
+			break;
+		case DRM_BLANK_UNBLANK:
+			if (screen_on)
+				break;
+			screen_on = true;
+			break;
+		default:
+			break;
+		}
+	}
+
+	return NOTIFY_OK;
+}
+
+static struct notifier_block dsi_panel_dc_dim_notifier = {
+	.notifier_call = dsi_panel_dc_dim_notifier_callback,
+};
+
+static int __init dsi_panel_dc_dim_init(void)
+{
+	int ret = 0;
+
+	// Register the driver module as a client of the DRM PANEL event notifier
+	ret = drm_register_client(&dsi_panel_dc_dim_notifier);
+	if (ret)
+		DSI_ERR("Failed to register notifier, err: %d\n", ret);
+
+	return ret;
+}
+
+static void __exit dsi_panel_dc_dim_exit(void)
+{
+	// Unregister the driver module as a client of the DRM PANEL event notifier
+	drm_unregister_client(&dsi_panel_dc_dim_notifier);
+}
+
+module_init(dsi_panel_dc_dim_init);
+module_exit(dsi_panel_dc_dim_exit);
+#endif
 
 void dsi_set_backlight_control(struct dsi_panel *panel,
 			struct dsi_display_mode *adj_mode)